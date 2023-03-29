c0fb845e63d3ff5c5d65d92ba845ca61b08ada58
Dalui-chingu <gowrisubha44@gmail.com>
2023-03-29 21:31:34 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Let we see the output of JAVASCRIPT</h1>
+        <button onClick={window['alertMsg']}>Click me</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
